7e1a63195d445553c9fc596903011581df8920a5
Jatin Pandey <jatinpandey@gmail.com>
2025-01-03 17:26:02 +0530

    changed

--- server.js
+++ server.js
@@ -82,6 +82,6 @@ app.post('/coins/init', async (req, res) => {
 });
 
 // Start the server
-app.listen(PORT, () => {
+app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
